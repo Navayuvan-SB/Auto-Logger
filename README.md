f2b109d60357adc7941844e226958996303ba051
navayuvan-sb <ns.navayuvan@gmail.com>
2019-11-15 20:37:58 +0530

    Linux Support - added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Auto-Logger
-This python file is for the developers and the programmers who wish to log what they do.
+This python file is for the developers and the programmers who wish to log what they do with the datetime stamp.
+
+**Note - This program is currently under development and configured only for `linux distros`**
+
+## What are the Requirements?
+
+- Python 3.6 or above
+
+## How should I Install it?
+
+- There is no specific installation for this program. Clone the Repo and just run the python file.
+
+```
+python make_log.py
+```
+
+## How should I use this program?
+
+- Type the log you want to save.
+- Hit the `Enter button` **Once** to save the log and Type more.
+- Hit the `Enter button` **Twice** to terminate the program. 
+
+## Where to view my logs?
+
+- The logs which you have created are stored in the file **my_log.txt** inside the folder named **AutoLogger** in your **home** 
---
+++ make_log.py
@@ -0,0 +1,50 @@
+""" ==================================================
+                Auto Logging using Python
+=================================================== """
+
+
+# Import Statements
+from datetime import datetime
+import os.path
+
+# Creating AutoLogger Directory if not exist
+try:
+    os.mkdir(os.path.expanduser("~/AutoLogger"))
+
+except FileExistsError:
+    pass
+
+# Expanding the directory
+path = os.path.expanduser(os.path.join("~/AutoLogger/", "my_log.txt"))
+
+# Opening a file
+f = open(path, "a+")
+
+# Initialise Flag as True
+flag = True
+
+# Get the input and write in the file
+while(flag):
+    # datetime object containing current date and time
+    now = datetime.now()
+ 
+    # dd/mm/YY H:M:S
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+    # Read the input from user
+    log_text = input("Log:  ")
+
+    # Check if the end condition satisfied
+    if (log_text == ""):
+
+        # Set the Flag to False and close the opened file
+        flag = False
+        f.close()
+    
+    else:
+        # Format the text to write
+        text_to_write = dt_string + " \t " + log_text + "\n\n"
+
+        # Write to the file
+        f.write(text_to_write) 
+
